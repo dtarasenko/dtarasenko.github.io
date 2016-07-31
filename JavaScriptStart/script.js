b5ba06bbb01444143032593a1448de6d6a295535
Dmitriy Tarasenko (Freelancer) <dmitriy.tarasenko@live.co.uk>
2016-07-31 19:46:04 +0300

    Add solution to module 4 JavaScript

---
+++ JavaScriptStart/script.js
@@ -0,0 +1,15 @@
+(function () {
+
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+	for (var prop in names) {
+		var firstLetter = names[prop].charAt(0).toLowerCase();
+
+		if (firstLetter == "j") {
+			byeSpeaker.speak(names[prop]);
+		} else {
+			helloSpeaker.speak(names[prop]);
+		}
+	}
+
+}) ();
